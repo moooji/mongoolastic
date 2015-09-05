9c70ff37129c09675775d45cca2520d349638d55
moooji <steffen.straetz@gmail.com>
2015-09-05 20:00:37 +0200

    Refactor of renderMapping function so that it is more comprehendable

--- test/plugin.js
+++ test/plugin.js
@@ -5,28 +5,25 @@ var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
 var mongoose = require('mongoose');
 var plugin = require('../lib/plugin');
-//var errors = require('../lib/errors');
+var errors = require('../lib/errors');
 
 var expect = chai.expect;
 chai.use(chaiAsPromised);
 
 var host = 'localhost:9200';
 
-var testSchemaIndex = 'mongoolastic-test-schema';
-var TestSchema = new mongoose.Schema({
-  url: {
+var catSchemaIndex = 'mongoolastic-cat-schema';
+var CatSchema = new mongoose.Schema({
+  name: {
     type: String,
     required: true
   },
-  title: {
-    type: String
-  },
-  mediaType: {
+  hobby: {
     type: String
   }
 }, {_id: false});
 
-var testSchemaSettings = {
+var catSchemaSettings = {
   'index': {
     'analysis': {
       'filter': {
@@ -39,15 +36,62 @@ var testSchemaSettings = {
   }
 };
 
+var dogSchemaIndex = 'mongoolastic-dog-schema';
+var DogSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  color: {
+    type: String,
+    required: true
+  }
+}, {_id: false});
+
 describe('Plugin - Register', function() {
 
-  it('should register a schema', function() {
+  it('should register a schema with settings', function() {
 
     return expect(plugin.register(
-      TestSchema,
-      testSchemaIndex,
-      testSchemaSettings))
+      CatSchema,
+      catSchemaIndex,
+      catSchemaSettings))
       .to.eventually.be.fulfilled;
+  });
+
+  it('should register a schema without settings', function() {
+
+    return expect(plugin.register(
+      DogSchema,
+      dogSchemaIndex))
+      .to.eventually.be.fulfilled;
+  });
+
+  it('should throw an InvalidArgumentError if index is not valid', function() {
+
+    return expect(plugin.register(
+      CatSchema,
+      123,
+      catSchemaSettings))
+      .to.be.rejectedWith(errors.InvalidArgumentError);
+  });
+
+  it('should throw an InvalidArgumentError if settings are not valid', function() {
+
+    return expect(plugin.register(
+      CatSchema,
+      catSchemaIndex,
+      123))
+      .to.be.rejectedWith(errors.InvalidArgumentError);
+  });
+
+  it('should throw an InvalidArgumentError if schema is not valid', function() {
+
+    return expect(plugin.register(
+      123,
+      catSchemaIndex,
+      catSchemaSettings))
+      .to.be.rejectedWith(errors.InvalidArgumentError);
   });
 });
 
